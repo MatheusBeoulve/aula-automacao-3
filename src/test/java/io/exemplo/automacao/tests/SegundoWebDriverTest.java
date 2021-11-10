5525d4c027bdb23438c91c913e341916c27aff38
Matheus H G Santos <matheus.beoulve@outlook.com.br>
2021-11-10 20:05:47 -0300

    Criado o codigo de exemplo para a segunda aula

---
+++ src/test/java/io/exemplo/automacao/tests/SegundoWebDriverTest.java
@@ -0,0 +1,43 @@
+package io.exemplo.automacao.tests;
+
+import io.exemplo.automacao.extensions.WebDriverExtension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(WebDriverExtension.class)
+public class SegundoWebDriverTest {
+
+    @Test
+    public void segundaAula(WebDriver webDriver) {
+        webDriver.get("https://matheusbeoulve.github.io/aulas/aula2/");
+
+        webDriver.findElement(By.linkText("Carro")).click();
+
+        WebElement tipoTransacao = webDriver.findElement(By.cssSelector("[name=tipo-transacao]"));
+        new Select(tipoTransacao).selectByIndex(2);
+        webDriver.findElement(By.cssSelector("[name=valor-veiculo]")).sendKeys("10000");
+        webDriver.findElement(By.cssSelector("[name=nome-emissor")).sendKeys("Matheus Henrique G Santos");
+        webDriver.findElement(By.cssSelector("[name=nome-beneficiario]")).sendKeys("Josefa Silene S Galdino");
+        webDriver.findElement(By.cssSelector("[name=ano-carro]")).sendKeys("2020");
+        webDriver.findElement(By.cssSelector("#carro-assegurado")).click();
+        webDriver.findElement(By.cssSelector("#usou-seguro-carro")).click();
+
+        webDriver.findElement(By.cssSelector("[type=submit]")).click();
+
+        String mensagem = webDriver.findElement(By.cssSelector("#confirmar-dados")).getText();
+
+        assertEquals("Obrigado, Matheus Henrique G Santos pelo seu pedido!", mensagem);
+    }
+
+    @Test
+    public void segundoDesafio(WebDriver webDriver) {
+        // Escreva aqui o codigo do seu desafio
+    }
+}
